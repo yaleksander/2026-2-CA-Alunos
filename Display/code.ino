833bbb3c840828e3cce4a8c6903fe25e53f1d4d0
Russo <yaleksander42@gmail.com>
2026-09-16 19:17:31 -0300

    atualizacao

--- Display/code.ino
+++ Display/code.ino
@@ -3,10 +3,7 @@
 #include <Adafruit_SSD1306.h>
 #include <LiquidCrystal_AIP31068_I2C.h>
 
-#define DISPLAYS 3
-
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
-LiquidCrystal_AIP31068_I2C lcd(0x3E, 20, 4);
 
 void setup()
 {
